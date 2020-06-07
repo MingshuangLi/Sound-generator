4a312235e4abd9f92e4964238ef12e3701f6ecbc
Mingshuang Li <44843611+MingshuangLi@users.noreply.github.com>
2020-06-07 16:53:51 -0500

    Update pink_noise.m

--- pink_noise.m
+++ pink_noise.m
@@ -2,4 +2,8 @@
 fs = 44100;
 duration = input('Please input the duration of pink noise (s): ');
 y = pinknoise(duration*fs);
+figure;
+pspectrum(y,fs);
+names= input('Please type name of new audiofile: ', 's');
+audiowrite(names,y,fs)
 
