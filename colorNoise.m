c832ccc776be4db57bbe02ef279b3cc1d00852c9
Mingshuang Li <44843611+MingshuangLi@users.noreply.github.com>
2020-06-07 17:25:36 -0500

    Add files via upload

---
+++ colorNoise.m
@@ -0,0 +1,14 @@
+% Mingshuang Li, UT Austin, 2020.06.07
+% a for colored noise: pink =1, white = 0, brown = -1, purple = -2 
+fs = input('Please input the sampling frequency (Hz): ')
+a = input('Please input the a (e.g., pink =1, white = 0, brown = -1, purple = -2): '); 
+duration = input('Please input the duration (second): ')
+cNoise = dsp.ColoredNoise(a,fs*duration);
+rng default;
+x = cNoise();
+x = x./(max(abs(x)));
+figure;
+pspectrum(x,fs);
+names= input('Please type name of new audiofile: ', 's');
+audiowrite(names,x,fs)
+
